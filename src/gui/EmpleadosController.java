afe904892ba7523c2155730835e14e9bf3750822
Carlos Vargas <varlopecar@gmail.com>
2021-10-31 17:55:24 -0600

    Módulo empleados sin terminar

---
+++ src/gui/EmpleadosController.java
@@ -0,0 +1,30 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+
+public class EmpleadosController implements Initializable {
+
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+
+	@FXML
+	private void agregar(ActionEvent event) throws IOException {
+		Scene currentWindow = ((Node) event.getTarget()).getScene();
+		ScrollPane mainContainer = (ScrollPane) currentWindow.lookup("#mainContainer");
+		Node nodo = FXMLLoader.load(getClass().getResource("ProductosForm.fxml"));
+		mainContainer.setContent(nodo);
+	}
+
+}
